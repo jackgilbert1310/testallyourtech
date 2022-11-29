4522fa3bb92cb40b11480e89a3aee14c7a5f5089
jack <jackgilbert1310@gmail.com>
2022-11-29 15:04:27 +0000

    Updated views

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,1 +1,19 @@
-test
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    
+    <div id="app">
+        <h1>Test</h1>
+
+        <a href="/home">Home</a> | <a href="/test">Test</a>  
+    </div>
+
+</body>
+</html>
+
